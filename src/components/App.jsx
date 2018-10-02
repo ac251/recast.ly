5079bb338670f358d82c38983debecc2ad588e5d
ac251 <aaron.d.cohn@gmail.com>
2018-10-02 14:45:27 -0700

    Bare minimums done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+//import searchYouTube from '../lib/searchYouTube.js';
+import Search from './Search.js';
+import YOUTUBE_API_KEY from '../config/youtube.js';
 
 /*var App = () => (
   <div>
@@ -25,14 +28,26 @@ import VideoPlayer from './VideoPlayer.js';
 // export default App;
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.changeVideo = this.changeVideo.bind(this);
+  constructor(props) {
+    super(props);
+    this.lastStr = '';
     this.state = {
-      videos: exampleVideoData,
-      video: exampleVideoData[0],
+      videos: [{snippet: {title: null, description: null, thumbnails: {default: {url: null}} }, id: {videoId: null}}],
+      video: {snippet: {title: null, description: null, thumbnails: {default: {url: null}} }, id: {videoId: null}},
+      search: '',
+      lastSearch: 0,
     };
 
+  }
+  runSearch() {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: this.state.search,
+      maxResults: 5
+    };
+    this.props.searchYouTube(options, (data) => {
+      this.setState({ videos: data, lastSearch: Date.now() });
+    });
   }
   
   changeVideo(video) {
@@ -41,27 +56,58 @@ class App extends React.Component {
     });
   }
 
+  updateSearchTerm(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+  
+  submitSearchTerm() {
+    this.runSearch();
+  }
+  
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search value={this.state.value} update={(event)=>this.updateSearchTerm(event)} submit={()=>this.submitSearchTerm()}/>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em><VideoPlayer video={this.state.video} /></h5></div>
+            <VideoPlayer video={this.state.video} />
           </div>
           <div className="col-md-5">
-            <div><h5><em>videoList</em><VideoList videos={this.state.videos}  onClick={this.changeVideo} /></h5></div>
+            <VideoList videos={this.state.videos} onClick={(video) =>this.changeVideo(video)} />
           </div>
         </div>
       </div>
     );
-
   }
 
+  componentDidMount() {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: 'cats',
+      maxResults: 5
+    };
+    this.props.searchYouTube(options, (data)=> {
+      this.setState({ videos: data, video: data[0] });
+    });
+  }
+  componentDidUpdate(prevState) {
+    var waitTime = 500;
+    if (this.state.search) {    
+      if ((this.state.search !== prevState.search) && Date.now() - this.state.lastSearch > waitTime) {
+        this.runSearch();
+      } else {
+        setTimeout( () => { this.runSearch(); }, waitTime - (Date.now() - this.state.lastSearch));
+      }
+    }
+  }
+  
+  
 }
 
 export default App;
